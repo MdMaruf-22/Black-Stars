69d2771a1c611cf6f833f4825f2619f6fc50e35f
Maruf <marufislam74208@gmail.com>
2025-03-19 16:58:22 +0600

    some fix

--- frontend/efootball_frontend/lib/main.dart
+++ frontend/efootball_frontend/lib/main.dart
@@ -6,19 +6,6 @@ import 'package:firebase_core/firebase_core.dart';
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   // await Firebase.initializeApp();
-  if(kIsWeb){
-    await Firebase.initializeApp(options: const FirebaseOptions(apiKey: "AIzaSyAN24-g6IlyCIHpmXinKagzEKRH2TQj7S0",
-        authDomain: "black-stars-f4fbb.firebaseapp.com",
-        databaseURL: "https://black-stars-f4fbb-default-rtdb.asia-southeast1.firebasedatabase.app",
-        projectId: "black-stars-f4fbb",
-        storageBucket: "black-stars-f4fbb.firebasestorage.app",
-        messagingSenderId: "49595679109",
-        appId: "1:49595679109:web:723ad133d6e2c1b20344ec",
-        measurementId: "G-PT0KQT3B5M"));
-  }
-  else{
-    await Firebase.initializeApp();
-  }
   runApp(MyApp());
 }
 
